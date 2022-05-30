c8aeae7f5b4943893b78773c0756bf5592548b03
saeed qhtani <89229794+SAEED5OBR@users.noreply.github.com>
2022-05-30 23:32:39 +0300

    Add files via upload

--- iqqhtani/utils/startup.py
+++ iqqhtani/utils/startup.py
@@ -42,11 +42,11 @@ async def load_plugins(folder):
                     os.remove(Path(f"iqqhtani/{folder}/{shortname}.py"))
             except Exception as e:
                 os.remove(Path(f"iqqhtani/{folder}/{shortname}.py"))
-                LOGS.info(f"♛ ︙غير قادر على التحميل {shortname} يوجد هناك خطا بسبب : {e}"                )
+                LOGS.info(f"◈ ︙غير قادر على التحميل {shortname} يوجد هناك خطا بسبب : {e}"                )
 async def startupmessage():
     try:
         if BOTLOG:
-            Config.CATUBLOGO = await iqqhtani.tgbot.send_file(BOTLOG_CHATID, "https://up6.cc/2022/05/165152347887371.jpg", caption="♛ ⦙ تـمّ  اعـادة تشـغيل\n القحطاني ✓  :  [ 7.6 ] .\n\n♛ ⦙ للحصول على اوامر السورس\n أرسـل : (  `.اوامري`  ) \n\n♛ ⦙ لمـعرفة كيفية تغير بعض كلايش\n او صور السـورس  أرسـل  :\n (  `.مساعده`  )\n\n♛ ⦙ القناة الرسمية القحطاني : @ghtanisaeed\n♛ ⦙ فارات سورس القحطاني :@ghtanisaeed \n♛ ⦙ كلايش القحطاني:  @ghtanisaeed\n ♛ ⦙التحديثات والاضافات :  @ghtanisaeed\n",                buttons=[(Button.url("مطور القحطاني الرسمي", "https://t.me/SAEED5OBR"),)],            )
+            Config.CATUBLOGO = await iqqhtani.tgbot.send_file(BOTLOG_CHATID, "https://up6.cc/2022/05/165152347887371.jpg", caption="◈ ⦙ تـمّ  اعـادة تشـغيل\n القحطاني ✓  :  [ 7.6 ] .\n\n◈ ⦙ للحصول على اوامر السورس\n أرسـل : (  `.اوامري`  ) \n\n◈ ⦙ لمـعرفة كيفية تغير بعض كلايش\n او صور السـورس  أرسـل  :\n (  `.مساعده`  )\n\n◈ ⦙ القناة الرسمية القحطاني : @ghtanisaeed\n◈ ⦙ فارات سورس القحطاني :@ghtanisaeed \n◈ ⦙ كلايش القحطاني:  @ghtanisaeed\n ◈ ⦙التحديثات والاضافات :  @ghtanisaeed\n",                buttons=[(Button.url("مطور القحطاني الرسمي", "https://t.me/SAEED5OBR"),)],            )
     except Exception as e:
         LOGS.error(e)
         return None
@@ -67,7 +67,7 @@ async def setup_bot():
         for option in config.dc_options:
             if option.ip_address == iqqhtani.session.server_address:
                 if iqqhtani.session.dc_id != option.id:
-                    LOGS.warning(                        f"♛ ︙ معرف DC ثابت في الجلسة من {iqqhtani.session.dc_id}"                        f"♛ ︙ يتبع ل {option.id}"                    )
+                    LOGS.warning(                        f"◈ ︙ معرف DC ثابت في الجلسة من {iqqhtani.session.dc_id}"                        f"◈ ︙ يتبع ل {option.id}"                    )
                 iqqhtani.session.set_dc(option.id, option.ip_address, option.port)
                 iqqhtani.session.save()
                 break
@@ -88,40 +88,40 @@ async def verifyLoggerGroup():
             entity = await iqqhtani.get_entity(BOTLOG_CHATID)
             if not isinstance(entity, types.User) and not entity.creator:
                 if entity.default_banned_rights.send_messages:
-                    LOGS.info(                        "♛ ︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."                    )
+                    LOGS.info(                        "◈ ︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."                    )
                 if entity.default_banned_rights.invite_users:
-                    LOGS.info(                        "♛ ︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."                    )
+                    LOGS.info(                        "◈ ︙الفار الأذونات مفقودة لإرسال رسائل لـ PRIVATE_GROUP_BOT_API_ID المحدد."                    )
         except ValueError:
-            LOGS.error("♛ ︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
+            LOGS.error("◈ ︙تـأكد من فـار المجـموعة  PRIVATE_GROUP_BOT_API_ID.")
         except TypeError:
-            LOGS.error(                "♛ ︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."            )
+            LOGS.error(                "◈ ︙لا يمكـن العثور على فار المجموعه PRIVATE_GROUP_BOT_API_ID. تأكد من صحتها."            )
         except Exception as e:
-            LOGS.error(                "♛ ︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"                + str(e)            )
+            LOGS.error(                "◈ ︙حدث استثناء عند محاولة التحقق من PRIVATE_GROUP_BOT_API_ID.\n"                + str(e)            )
     else:
-        descript = "♛ ︙ لا تحذف هذه المجموعة أو تغير إلى مجموعة (إذا قمت بتغيير المجموعة ، فسيتم فقد كل شيئ .)"
+        descript = "◈ ︙ لا تحذف هذه المجموعة أو تغير إلى مجموعة (إذا قمت بتغيير المجموعة ، فسيتم فقد كل شيئ .)"
         _, groupid = await create_supergroup(            "تخزين القحطاني العام", iqqhtani, Config.TG_BOT_USERNAME, descript        )
         addgvar("PRIVATE_GROUP_BOT_API_ID", groupid)
-        print("♛ ︙ تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
+        print("◈ ︙ تم إنشاء مجموعة المسـاعدة بنجاح وإضافتها إلى المتغيرات.")
         flag = True
     if PM_LOGGER_GROUP_ID != -100:
         try:
             entity = await iqqhtani.get_entity(PM_LOGGER_GROUP_ID)
             if not isinstance(entity, types.User) and not entity.creator:
                 if entity.default_banned_rights.send_messages:
-                    LOGS.info(                        "♛ ︙ الأذونات مفقودة لإرسال رسائل لـ PM_LOGGER_GROUP_ID المحدد."                    )
+                    LOGS.info(                        "◈ ︙ الأذونات مفقودة لإرسال رسائل لـ PM_LOGGER_GROUP_ID المحدد."                    )
                 if entity.default_banned_rights.invite_users:
-                    LOGS.info(                        "♛ ︙الأذونات مفقودة للمستخدمين الإضافيين لـ PM_LOGGER_GROUP_ID المحدد."                    )
+                    LOGS.info(                        "◈ ︙الأذونات مفقودة للمستخدمين الإضافيين لـ PM_LOGGER_GROUP_ID المحدد."                    )
         except ValueError:
-            LOGS.error("♛ ︙ لا يمكن العثور على فار  PM_LOGGER_GROUP_ID. تأكد من صحتها.")
+            LOGS.error("◈ ︙ لا يمكن العثور على فار  PM_LOGGER_GROUP_ID. تأكد من صحتها.")
         except TypeError:
-            LOGS.error("♛ ︙ PM_LOGGER_GROUP_ID غير مدعوم. تأكد من صحتها.")
+            LOGS.error("◈ ︙ PM_LOGGER_GROUP_ID غير مدعوم. تأكد من صحتها.")
         except Exception as e:
-            LOGS.error(                "♛ ︙ حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)            )
+            LOGS.error(                "◈ ︙ حدث استثناء عند محاولة التحقق من PM_LOGGER_GROUP_ID.\n" + str(e)            )
     else:
-        descript = "♛ ︙ وظيفه هذا المجموعة لحفض رسائل التي تكون موجة اليك ان لم تعجبك هذا المجموعة قم بحذفها نهائيأ 👍 \n  الـسورس : - @Iqqhtani"
+        descript = "◈ ︙ وظيفه هذا المجموعة لحفض رسائل التي تكون موجة اليك ان لم تعجبك هذا المجموعة قم بحذفها نهائيأ 👍 \n  الـسورس : - @Iqqhtani"
         _, groupid = await create_supergroup(            "تخزين القحطاني الخاص", iqqhtani, Config.TG_BOT_USERNAME, descript        )
         addgvar("PM_LOGGER_GROUP_ID", groupid)
-        print("♛ ︙ تم إنشاء مجموعة خاصة لـ PRIVATE_GROUP_BOT_API_ID بنجاح وإضافتها إلى المتغيرات.")
+        print("◈ ︙ تم إنشاء مجموعة خاصة لـ PRIVATE_GROUP_BOT_API_ID بنجاح وإضافتها إلى المتغيرات.")
         flag = True
     if flag:
         executable = sys.executable.replace(" ", "\\ ")
